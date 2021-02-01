b75a0f6f32abe8718fde8adc8d23169e84a4dbdf
ymasog <yer.masog@gmail.com>
2021-01-31 23:22:22 -0600

    added newIntern and newEngineer functions

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 //questions for employee
-        //.then >> ceate Employee object
-                //type of employee:
-                        //if manager >> create manager object
-                                //else intern >> create intern object 
+//.then >> ceate Employee object
+//type of employee:
+//if manager >> create manager object
+//else intern >> create intern object 
 //write file with objects
 const team = [];
 const employeeRole = [
@@ -27,129 +27,130 @@ const employeeRole = [
 
 function role() {
     inquirer.prompt(employeeRole)
-    .then((response) => {
-        let role = ""
-        
-        switch (response) {
-         case "Manager" : role = "Manager"
-            break;
-         case "Engineer" : role = "Engineer"  
-            break;
-         case "Intern" : role = "Intern"
-            break;
-         default: "Please choose a role"
-         
-         return role
-        }
-    });
-    newEmployee();
-    
+        .then((response) => {
+            let empRole = ""
+            switch (response) {
+                case "Manager": empRole = "Manager"
+                    break;
+                case "Engineer": empRole = "Engineer"
+                    break;
+                case "Intern": empRole = "Intern"
+                    break;
+                default: "Please choose a role"
+
+                    return empRole
+
+            }
+            
+        })
+
 }
 
 function newEmployee(role) {
-    if(role=="Manager") {
+    if (role == "Manager") {
         newManager();
     }
-    else if(role=="Engineer") {
+    else if (role == "Engineer") {
         newEngineer();
-    } 
-    else if(role=-"Intern") {
+    }
+    else if (role = -"Intern") {
         newIntern();
     }
-    
+
 }
 
 function newManager() {
-    inquirer.prompt(managerQuestions)
-    .then(response => {
-        let manager = new Manager(response.name, response.id, response.email, response.office)
-        team.push(manager)
-    }),
-}
-
-function newEngineer() {
-    inquirer.prompt(engineerQuestions)
-    .then(response => {
-        let engineer = new Engineer(response.name, response.id, response.email, response.github)
-        team.push(engineer)
-    }),
-}
-
-function newIntern() {
-    inquirer.prompt(internQuestions)
-    .then(response => {
-        let intern = new Intern(response.name, response.id, response.email, response.school)
-        team.push(intern)
-    }),
+    inquirer
+        .prompt(managerQuestions)
+        .then((response) => {
+            let manager = new Manager(response.name, response.id, response.email, response.office)
+            team.push(manager)
+        })
 }
 
 const managerQuestions = [
-        {
-            type: "input",
-            message: "Name:",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "ID",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "Email address:",
-            name: "email"
-        },
-        {
-            type: "list",
-            message: "Office number:",
-            name: "office"
-        },
-    ];
-    
-    const engineerQuestions = [
-        {
-            type: "input",
-            message: "Name:",
-            name: "engineer name"
-        },
-        {
-            type: "input",
-            message: "ID:",
-            name: "engineer id"
-        },
-        {
-            type: "input",
-            message: "Email address:",
-            name: "engineer email"
-        },
-        {
-            type: "input",
-            message: "GitHub username",
-            name: "github"
-        },
-    ];
-    
-    const internQuestions = [
-        {
-            type: "input",
-            message: "Name:",
-            name: "intern name"
-        },
-        {
-            type: "input",
-            message: "ID:",
-            name: "intern id"
-        },
-        {
-            type: "input",
-            message: "Email address:",
-            name: "intern email"
-        },
-        {
-            type: "input",
-            message: "School",
-            name: "school"
-        },
-    ];
-    
-    
+    {
+        type: "input",
+        message: "Name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "ID",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Email address:",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "Office number:",
+        name: "office"
+    },
+];
+
+function newEngineer() {
+    inquirer.prompt(engineerQuestions)
+        .then(response => {
+            let engineer = new Engineer(response.name, response.id, response.email, response.github)
+            team.push(engineer)
+        })
+}
+
+const engineerQuestions = [
+    {
+        type: "input",
+        message: "Name:",
+        name: "engineer name"
+    },
+    {
+        type: "input",
+        message: "ID:",
+        name: "engineer id"
+    },
+    {
+        type: "input",
+        message: "Email address:",
+        name: "engineer email"
+    },
+    {
+        type: "input",
+        message: "GitHub username",
+        name: "github"
+    },
+];
+
+function newIntern() {
+    inquirer.prompt(internQuestions)
+        .then(response => {
+            let intern = new Intern(response.name, response.id, response.email, response.school)
+            team.push(intern)
+        })
+}
+
+const internQuestions = [
+    {
+        type: "input",
+        message: "Name:",
+        name: "intern name"
+    },
+    {
+        type: "input",
+        message: "ID:",
+        name: "intern id"
+    },
+    {
+        type: "input",
+        message: "Email address:",
+        name: "intern email"
+    },
+    {
+        type: "input",
+        message: "School",
+        name: "school"
+    },
+];
+
+role();
